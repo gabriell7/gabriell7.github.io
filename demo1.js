5e8f1be337bef5eb02370fb5f95fb1e72fab9561
gabriell7 <mupapa89@domain.com>
2020-08-12 14:15:50 +0300

    12.8 harjoitus

---
+++ demo1.js
@@ -0,0 +1,7 @@
+let admin;//muuttujan esittely
+let nimi;//muuttujan esittely
+
+nimi = "Juha"//muuttujalle arvo
+admin = nimi;//muuttujan arvo sijoitetaan toiselle muuttujalle
+
+console.log (admin);//tulostetaan konsoliin muuttujan admin arvo
