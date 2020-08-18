41c796d702cb745d72c092bae85fee5e7578beae
gabriell7 <mupapa89@domain.com>
2020-08-18 09:04:51 +0300

    korjaus 18.8

--- demo1.js
+++
@@ -1,7 +0,0 @@
-let admin;//muuttujan esittely
-let nimi;//muuttujan esittely
-
-nimi = "Juha"//muuttujalle arvo
-admin = nimi;//muuttujan arvo sijoitetaan toiselle muuttujalle
-
-console.log (admin);//tulostetaan konsoliin muuttujan admin arvo
